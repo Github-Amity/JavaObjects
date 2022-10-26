7685e367c09b1e78b66e18002099ff54b64b11f9
Tarctic <zainabshah2003@gmail.com>
2022-10-26 11:03:19 +0300

    Add project files

---
+++ Student.java
@@ -0,0 +1,34 @@
+public class Student {
+
+    // Attributes
+    String username;
+    int age;
+
+    // Method
+    public void printInfo() {
+        System.out.println("Username: " + this.username);           // Print object attributes
+        System.out.println("Age: " + this.age);
+        System.out.println("--------------------------------");  // Separator for aesthetics
+    }
+
+    public static void main(String[] args) {
+
+        // Example
+        Student example = new Student(); // Creating an object of Class Student
+        example.username = "Example";    // Setting values for the object
+        example.age = 20;
+        example.printInfo();             // Calling a method on the object
+
+        // Tarctic
+        Student tarctic = new Student();
+        tarctic.username = "Tarctic";
+        tarctic.age = 18;
+        tarctic.printInfo();
+
+
+
+
+        /* Add your code above this line */
+
+    }
+}
